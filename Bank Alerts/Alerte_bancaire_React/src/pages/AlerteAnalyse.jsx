786f0849237ea5e5f92956936dbdf87b9835f5f1
Yassine Emhamed <72286417+yassine2med@users.noreply.github.com>
2025-01-20 00:39:29 -0500

    Delete Bank Alerts directory

--- Bank Alerts/Alerte_bancaire_React/src/pages/AlerteAnalyse.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import StepperAlerteAnalyse from '../components/stepper/StepperAlerteAnalyse'
-
-export default function AlerteAnalyse() {
-  return (
-    <StepperAlerteAnalyse/> 
-  )
-}
-
